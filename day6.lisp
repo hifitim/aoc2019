3bb9f75dbe43342ca298d44d2217952d81cac213
hifitim <tim.hifi@gmail.com>
2020-05-20 11:48:35 -0500

    Add files via upload
    started work on hash-table implementation.

--- day6.lisp
+++ day6.lisp
@@ -1,104 +1,86 @@
-;; ('COM '('B '('G 'H)) 'C '('D 'I) '('E '('J '('K 'L))) 'F)
-
 (defun read-input (file-name)
-	(let ((ret nil))
-		(with-open-file (stream
-			file-name
-			:direction :input)
-		(loop for line = (read-line stream nil)
-			while line do (push (parse-orbit line) ret)))
-		ret))
-
-(defun parse-orbit (orbit)
-  (let* ((split-orbit (split-by-delimiter orbit #\)))
-   (a (car split-orbit))
-   (b (cdr split-orbit)))
-  (cons a b)))
-
+  (with-open-file (stream
+		   file-name
+		   :direction :input)
+    (loop for line = (read-line stream nil)
+       while line collect
+	 (cons
+	  (car (split-by-delimiter line #\)))
+	  (cadr (split-by-delimiter line #\)))))))
+      
 (defun split-by-delimiter (string delimiter)
   "Split a string into a list on <delimiter>."
   (declare (type string string))
   (declare (type character delimiter))
   (loop for i = 0 then (1+ j)
-   as j = (position delimiter string :start i)
-   collect (subseq string i j)
-   while j))
+     as j = (position delimiter string :start i)
+     collect (subseq string i j)
+     while j))
 
-(defun eql-com (item1 item2)
-  (if (string= (car item1) "COM")
-    t
-    nil))
+(defun graph-sorter (x y)
+  (cond
+    ((string-equal (car x) "COM")
+     t)
+    ((string-equal (car y) "COM")
+     nil)
+    ((string< (car x) (car y))
+     t)
+    ((string-equal (car x) (car y))
+     (string< (cdr x) (cdr y)))
+    (t
+     nil)))
 
-(defun orbit-list-sort (olist)
-  (sort (sort (sort olist #'cadr-string<) #'car-string<) #'eql-com))
+(defun print-hash-entry (key value)
+  (format t "key: ~A, value: ~A~%" key value))
 
-(defun car-string< (item1 item2)
-  (and
-    (string< (car item1) (car item2))))
-
-(defun cadr-string< (item1 item2)
-  (and
-    (string< (cadr item1) (cadr item2))))
-
-(defun generate-graph (orbit-list)
+(defun exists-in-graph (item graph)
   (labels
-    ((graph-recurse (lst ret)
-      (let ((next (car lst)))
-        (cond
-          ((null lst)
-           ret)
-          ((string= "COM" (car next))
-           (setf ret (make-hash-table :test #'equal))
-           (setf (gethash "COM" ret) (cadr next))
-           (graph-recurse
-             (cdr lst)
-             ret))
+      ((graph-recurse (item keys graph)
+	 (cond
+	   ((and
+	     (null keys)
+	     (> (hash-key-count graph) 0))
+	    (graph-recurse
+	     item
+	     (hash-keys (gethash (car (hash-keys graph)) graph))
+	     (gethash (car (hash-keys graph)) graph)))
+	   ((null keys)
+	    nil)
+	   ((equal item (car keys))
+	    (gethash (car keys) graph))
+	   (t
+	    (graph-recurse item (cdr keys) graph)))))
+    (graph-recurse item (hash-keys graph) graph)))
+  
+(defun add-to-graph (item graph)
+  t)
 
-          (t
-            ret)))))
-    (let ((olist (orbit-list-sort orbit-list)))
-      (format t "olist: ~A~%~%" olist)
-      (graph-recurse olist nil))))
+(defun build-graph (input)
+  (labels
+      ((build-recurse (input graph)
+	 (cond
+	   ((string-equal (caar input) "COM")
+	    t) ;; special case TBD for first node.
+	   ((exists-in-graph (caar input) graph)
+	    t) ;; not sure what to do yet.
+	   (t ;; doesn't exist in graph
+	    (build-recurse
+	     (cdr input)
+	     (add-to-graph
+	      (car input)
+	      graph))))))
+    (build-recurse input nil)))
 
-(defun exists-in-hash-table (item table)
-  (labels ((hash-recurse (item table ret)
-                         (loop for k being the hash-keys in table using (hash-value v)
-                               do
-                               (print-hash-table table)
-                               (if (equal k item)
-                                   t
-                                   (when (eql (type-of v) 'hash-table)
-                                     (hash-recurse item v ret))))))
-          (hash-recurse item table nil)))
+(defun hash-keys (hash-table)
+  (loop for key being the hash-keys of hash-table collect key))
 
-(defun print-hash-table (table)
-  (loop for k being the hash-keys in table using (hash-value v)
-        do
-        (format t "~A: ~A~%" k v)))
-
-(defun print-hash-table-recurse (table)
-  (labels ((hash-recurse (table level)
-                         (loop for k being the hash-keys in table using (hash-value v)
-                               do
-                               (loop for k from 0 upto (* level 2)
-                                     do
-                                     (format t " "))
-                               (format t "~A: ~A~%" k v)
-                               (if (equal (type-of v) 'hash-table)
-                                   (hash-recurse v (+ level 1))))))
-          (hash-recurse table 0)))
-
-  (defun remove-at (lst idx)
-    "Generate new list that's equal to <lst>, except that the item at <idx> is gone."
-    (concatenate
-     'list
-     (subseq lst 0 idx)
-     (nthcdr (+ idx 1) lst)))
-
-  (defun replace-at (lst idx n)
-    "Generate new list that's equal to <lst>, except that <n> is now at position <idx>."
-    (concatenate
-     'list
-     (subseq lst 0 idx)
-     (list n)
-     (nthcdr (+ idx 1) lst)))
+(defun hash-key-count (hash-table)
+  (length (hash-keys hash-table)))
+       
+(defparameter *COM* (make-hash-table :test #'equal))
+(defun build-test ()
+  (setf (gethash "B" *COM*) (make-hash-table :test #'equal))
+  (setf (gethash "C" (gethash "B" *COM*)) (make-hash-table :test #'equal))
+  (setf (gethash "G" (gethash "B" *COM*)) (make-hash-table :test #'equal))
+  (setf (gethash "H" (gethash "G" (gethash "B" *COM*))) (make-hash-table :test #'equal)))
+  
