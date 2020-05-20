c342c79428491d40dd74cbe72fdf9c9c35d6dd1c
hifitim <tim.hifi@gmail.com>
2020-05-20 16:52:33 -0500

    exists-in-graph add-to-graph and print-graph work!

--- day6.lisp
+++ day6.lisp
@@ -30,8 +30,23 @@
     (t
      nil)))
 
-(defun print-hash-entry (key value)
-  (format t "key: ~A, value: ~A~%" key value))
+(defun print-hash-entry (key hash-table)
+  (format t "key: ~A, value: ~A~%" key (gethash key hash-table)))
+
+(defun print-depth (depth)
+  (loop for i from 0 upto (* depth 2)
+     do (format t " ")))
+
+(defun print-graph (graph)
+  (labels
+      ((print-internal (graph depth)
+	 (loop for k being each hash-key of graph
+	    do
+	      (print-depth depth)
+	      (format t "~A~%" k)
+	      (if (> (hash-key-count (gethash k graph)) 0)
+		  (print-internal (gethash k graph) (+ depth 1))))))
+    (print-internal graph 0)))
 
 (defun exists-in-graph (item graph)
   (labels
@@ -52,24 +67,28 @@
 	    (graph-recurse item (cdr keys) graph)))))
     (graph-recurse item (hash-keys graph) graph)))
   
-(defun add-to-graph (item graph)
-  t)
+(defun add-to-graph (item parent graph)
+  (let ((exists (exists-in-graph parent graph)))
+    (if exists
+	(setf (gethash item exists) (make-hash-table :test #'equal)))))
 
 (defun build-graph (input)
-  (labels
-      ((build-recurse (input graph)
-	 (cond
-	   ((string-equal (caar input) "COM")
-	    t) ;; special case TBD for first node.
-	   ((exists-in-graph (caar input) graph)
-	    t) ;; not sure what to do yet.
-	   (t ;; doesn't exist in graph
-	    (build-recurse
-	     (cdr input)
-	     (add-to-graph
-	      (car input)
-	      graph))))))
-    (build-recurse input nil)))
+  (let ((out-graph nil))
+    (labels
+	((build-recurse (input graph)
+	   (cond
+	     ((string-equal (caar input) "COM")
+	      t) ;; special case TBD for first node.
+	     ((exists-in-graph (caar input) graph)
+	      t) ;; not sure what to do yet.
+	     (t ;; doesn't exist in graph
+	      (build-recurse
+	       (cdr input)
+	       (add-to-graph
+		(car input)
+		graph))))))
+      (build-recurse input nil))
+    out-graph))
 
 (defun hash-keys (hash-table)
   (loop for key being the hash-keys of hash-table collect key))
@@ -82,5 +101,6 @@
   (setf (gethash "B" *COM*) (make-hash-table :test #'equal))
   (setf (gethash "C" (gethash "B" *COM*)) (make-hash-table :test #'equal))
   (setf (gethash "G" (gethash "B" *COM*)) (make-hash-table :test #'equal))
-  (setf (gethash "H" (gethash "G" (gethash "B" *COM*))) (make-hash-table :test #'equal)))
+  (setf (gethash "H" (gethash "G" (gethash "B" *COM*))) (make-hash-table :test #'equal))
+  (setf (gethash "Q" (gethash "B" *COM*)) (make-hash-table :test #'equal)))
   
